a4453789ed9887916bc8754ae0917e04b048ca7a
Daniel Bjerkehagen <Daniel.Bjerkehagen@sintef.no>
2021-08-06 09:02:28 +0200

    Format and document code

--- analysis.py
+++ analysis.py
@@ -13,22 +13,39 @@ The functions for interactively performing analysis should be structured as foll
 4. Interactively store results to file and/or sub-results-dictionary for ability to
 perform some other analysis on results later.
 """
-from network_modification import interactively_inspect_loads
 import network
 import load_points
 import plotting
 import utilities
 import numpy as np
 
-# Helper function
+# Helper functions
 
 
 def interactively_traverse_nested_dictionary(dict_choices):
+    """Traverse nested string-keyed dictionary
 
+    Parameters:
+    ----------
+    dict_choices : dict
+        Dictionary to traverse
+
+    Returns:
+    ----------
+    str_final_key : str
+        Final input key
+    data : 
+        Content at final input key
+
+    Notes:
+    ----------
+    Dictionary is traversed like a file-system, where each nested dictionary may
+    be interpreted as a sub-folder.
+    """
     print("\nAvailable files: ")
     print()
     utilities.print_dictionary_recursive(dict_choices)
-    
+
     bool_successful_input = False
     while not bool_successful_input:
         print("\nInput key of data-source to follow")
@@ -38,21 +55,43 @@ def interactively_traverse_nested_dictionary(dict_choices):
         else:
             bool_successful_input = True
     if type(dict_choices[str_key]) == dict:
-        str_final_key, data = interactively_traverse_nested_dictionary(dict_choices[str_key])
+        str_final_key, data = interactively_traverse_nested_dictionary(
+            dict_choices[str_key])
     else:
         str_final_key = str_key
         data = dict_choices[str_key]
     return str_final_key, data
 
 
-def interactively_insert_into_dictionary(dict_content, new_content, str_content_description):  # kan sikker kutte str_content_description
+def interactively_insert_into_dictionary(dict_content, new_content, str_content_description="content"):
+    """Input content to dictionary interactively
+
+    Parameters:
+    ----------
+    dict_content : dict
+        Dictionary to insert content into
+    new_content : 
+        Content to insert
+    str_content_description : str (optional)
+        Description of content, to be used in display
+
+    Returns:
+    ----------
+    dict_content : dict
+        Dictionary after insertion
+
+    Notes:
+    ----------
+    Leaving input blank aborts storing of new content.
+    """
     print("The following keys are already stored:")
     for key in dict_content:
         print(key)
 
     bool_successfully_input = False
     while not bool_successfully_input:
-        print("Input name to store", str_content_description, "under (leave blank to abort)")
+        print("Input name to store", str_content_description,
+              "under (leave blank to abort)")
         str_name = str(input())
         if not str_name:
             print("Aborting storage!")
@@ -74,7 +113,8 @@ def interactively_write_to_file_in_directory(str_results_directory_path, result)
 # Analysis
 
 def max_load(ts_load):
-    return np.max(ts_load[:,1])
+    return np.max(ts_load[:, 1])
+
 
 def aggregate_load_of_node(str_load_ID, dict_loads_ts, g_network):
     """Finds timeseries of total load experienced by a node.
@@ -107,11 +147,12 @@ def aggregate_load_of_node(str_load_ID, dict_loads_ts, g_network):
     try:
         ts_sum = dict_loads_ts[str(str_load_ID)]
     except KeyError:
-            print("Warning: Load-point", str_load_ID, "is missing timeseries!")
-            ts_sum = []
-    if list_children:    
+        print("Warning: Load-point", str_load_ID, "is missing timeseries!")
+        ts_sum = []
+    if list_children:
         for n_child in list_children:
-            ts_child = aggregate_load_of_node(n_child, dict_loads_ts, g_network)
+            ts_child = aggregate_load_of_node(
+                n_child, dict_loads_ts, g_network)
             ts_sum = load_points.add_timeseries(ts_sum, ts_child)
     return ts_sum
 
@@ -119,36 +160,43 @@ def aggregate_load_of_node(str_load_ID, dict_loads_ts, g_network):
 # Interactive analysis
 
 def interactive_max_load(dict_analysis_config, dict_results, dict_loads_ts):
+    """Interactively calculate max load of timeseries
+    """
     print("Beginning interactive max-load calculation")
 
-    #1. Choose data-source and parameters interactively.
+    # 1. Choose data-source and parameters interactively.
     print("Choose timeseries to calculate max load of:")
     str_ID, ts_load = interactively_traverse_nested_dictionary(
         {
-            "customers" : dict_loads_ts,
-            "results" : dict_results
+            "customers": dict_loads_ts,
+            "results": dict_results
         }
     )
 
-    #2. Perform analysis.
+    # 2. Perform analysis.
     fl_max_load = max_load(ts_load)
 
-    #3. Present results graphically or numerically.
-    print("Calculated the following max-load:", fl_max_load, "for load", str_ID)
+    # 3. Present results graphically or numerically.
+    print("Calculated the following max-load:",
+          fl_max_load, "for load", str_ID)
 
-    #4. Interactively store results to file and/or sub-results-dictionary for ability to
-    #perform some other analysis on results later.
-    dict_results = interactively_insert_into_dictionary(dict_results, fl_max_load, "max load")
+    # 4. Interactively store results to file and/or sub-results-dictionary for ability to
+    # perform some other analysis on results later.
+    dict_results = interactively_insert_into_dictionary(
+        dict_results, fl_max_load, "max load")
     str_results_directory_path = dict_analysis_config["result_storage_path"]
-    interactively_write_to_file_in_directory(str_results_directory_path, fl_max_load)
+    interactively_write_to_file_in_directory(
+        str_results_directory_path, fl_max_load)
 
     return dict_results
 
 
 def interactive_load_aggregation(dict_analysis_config, dict_results, dict_loads_ts, g_network):
+    """Interactively perform load-aggregation
+    """
     print("Beginning interactive load-aggregation")
-    
-    #1. Choose data-source and parameters interactively.
+
+    # 1. Choose data-source and parameters interactively.
     bool_successfully_input_node_of_network = False
     while not bool_successfully_input_node_of_network:
         print("Nodes in network: ")
@@ -159,25 +207,30 @@ def interactive_load_aggregation(dict_analysis_config, dict_results, dict_loads_
             bool_successfully_input_node_of_network = True
         else:
             print("Could not find", str_load_ID, "in network, try again!")
-    
-    #2. Perform analysis.
+
+    # 2. Perform analysis.
     ts_agg = aggregate_load_of_node(str_load_ID, dict_loads_ts, g_network)
 
-    #3. Present results graphically or numerically.
+    # 3. Present results graphically or numerically.
     print("Got the following aggregated load at node", str_load_ID)
     plotting.plot_timeseries(
-        [ts_agg], ["Aggregated load"], 
+        [ts_agg], ["Aggregated load"],
         "Time", "Load", "Aggregated load")
 
-    #4. Interactively store results to file and/or sub-results-dictionary for ability to
-    #perform some other analysis on results later.
-    dict_results = interactively_insert_into_dictionary(dict_results, ts_agg, "aggregated load")
+    # 4. Interactively store results to file and/or sub-results-dictionary for ability to
+    # perform some other analysis on results later.
+    dict_results = interactively_insert_into_dictionary(
+        dict_results, ts_agg, "aggregated load")
     str_results_directory_path = dict_analysis_config["result_storage_path"]
-    interactively_write_to_file_in_directory(str_results_directory_path, ts_agg)
+    interactively_write_to_file_in_directory(
+        str_results_directory_path, ts_agg)
 
     return dict_results
 
+
 def interactively_inspect_previous_results(dict_results):
+    """Interactively inspect results
+    """
     print("Beginning inspection of results")
 
     bool_continue = True
@@ -186,7 +239,8 @@ def interactively_inspect_previous_results(dict_results):
         print(key, ":", result)
 
         print("Continue (yes)/no")
-        str_choice = str.lower(utilities.input_until_expected_type_appears(str))
+        str_choice = str.lower(
+            utilities.input_until_expected_type_appears(str))
         if str_choice == "no" or str_choice == "n":
             bool_continue = False
 
@@ -211,9 +265,11 @@ def interactively_choose_analysis(dict_config, dict_results, dict_loads_ts, g_ne
         str_choice = input()
 
         if str_choice == '1':
-            dict_results = interactive_max_load(dict_analysis_config, dict_results, dict_loads_ts)
+            dict_results = interactive_max_load(
+                dict_analysis_config, dict_results, dict_loads_ts)
         elif str_choice == '2':
-            dict_results = interactive_load_aggregation(dict_analysis_config, dict_results, dict_loads_ts, g_network)
+            dict_results = interactive_load_aggregation(
+                dict_analysis_config, dict_results, dict_loads_ts, g_network)
         elif str_choice == '3':
             print("Not yet implemented")
         elif str_choice == '8':
