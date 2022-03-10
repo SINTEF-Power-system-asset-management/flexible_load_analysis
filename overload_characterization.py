9df4a0501c70bb78f7d29ed36c37d4c92f31d7c1
Daniel Bjerkehagen <daniel.bjerkehagen@sintef.no>
2022-03-10 10:48:23 +0100

    Cleanup random adding of additional loads

--- overload_characterization.py
+++ overload_characterization.py
@@ -8,42 +8,47 @@ import plotting
 import copy
 import numpy as np
 
-np.random.seed(0)
 
-STR_CONFIG_PATH = "ora_data/config_ORA.toml"
-dict_config, dict_data, dict_network = init.initialize_config_and_data(
-    STR_CONFIG_PATH)
+def add_random_loads_flex_analysis(loads, network, agg_index, num_iterations, 
+                                    plot_aggregate=True, plot_overload=True):
+    str_agg_id = network["branch"]["F_BUS"][agg_index]
+    fl_limit_kW = float(network["branch"]["RATE_A"][agg_index])*1000
 
-# Data-structure
-dict_loads_ts = load_points.prepare_all_loads(dict_config, dict_data)
+    all_load_ids = [load_id for load_id in loads]
+    l_loads_added = []
+
+    for i in range(num_iterations + 1):
+        if i != 0:
+            print("Adding new load to network...")
+            id_to_copy_from = np.random.choice(all_load_ids)
+            ts_new_load_data = copy.deepcopy(loads[id_to_copy_from])
+            net_modification.add_new_load_to_net(
+                "5000" + str(i), ts_new_load_data, str_agg_id, loads, network)
+            l_loads_added.append(id_to_copy_from)
+
+        if i % 10 == 0:
+            print(f"Loads added so far: {l_loads_added}")
+            ts_agg = load_aggregation.aggregate_load_of_node(
+                str_agg_id, loads, network)
+            if plot_aggregate: plotting.plot_timeseries([ts_agg], [
+                                     "Aggregated"], f"Aggregated load and limit after {i} addition(s)", fl_limit=fl_limit_kW)
+
+            l_overloads = flexibility_need.find_overloads(ts_agg, fl_limit_kW)
+            #l_overloads = flexibility_need.remove_unimportant_overloads(l_overloads)
+            if l_overloads:
+                flex_need = flexibility_need.FlexibilityNeed(l_overloads)
+                if plot_overload: flexibility_need.plot_flexibility_characteristics(flex_need)
 
 
-i_agg_ind = 1
-str_agg_id = dict_network["branch"]["F_BUS"][i_agg_ind]
-all_load_ids = [load_id for load_id in dict_loads_ts]
-l_loads_added = []
+if __name__ == "__main__":
 
-for i in range(101):
-    if i != 0:
-        print("Adding new load to network...")
-        id_to_copy_from = np.random.choice(all_load_ids) #"40016"
-        ts_new_load_data = copy.deepcopy(dict_loads_ts[id_to_copy_from])
-        """ Removed to instead add 10s of loads and aggregate statistics
-        fl_old_max_load = np.max(ts_new_load_data[:, 1])
-        fl_new_max_load = 1000
-        fl_scaling_factor = fl_new_max_load/fl_old_max_load
-        ts_new_load_data = ts.scale_timeseries(ts_new_load_data, fl_scaling_factor)
-        """
-        net_modification.add_new_load_to_net("5000" + str(i), ts_new_load_data, str_agg_id, dict_loads_ts, dict_network)
-        l_loads_added.append(id_to_copy_from)
+    np.random.seed(0)
 
-    if i % 10 == 0:
-        fl_lim_kW = float(dict_network["branch"]["RATE_A"][i_agg_ind])*1000
-        ts_agg = load_aggregation.aggregate_load_of_node(str_agg_id, dict_loads_ts, dict_network)
-        l_overloads = flexibility_need.find_overloads(ts_agg, fl_lim_kW)
-        #l_overloads = flexibility_need.remove_unimportant_overloads(l_overloads)
-        if l_overloads: flex_need = flexibility_need.FlexibilityNeed(l_overloads)
-        
-        print(l_loads_added)
-        plotting.plot_timeseries([ts_agg], ["Aggregated"], f"Aggregated load and limit after {i} addition(s)",fl_limit=fl_lim_kW)
-        if l_overloads: flexibility_need.plot_flexibility_characteristics(flex_need)
+    STR_CONFIG_PATH = "ora_data/config_ORA.toml"
+    dict_config, dict_data, dict_network = init.initialize_config_and_data(
+        STR_CONFIG_PATH)
+
+    # Data-structure
+    dict_loads_ts = load_points.prepare_all_loads(dict_config, dict_data)
+
+    #add_random_loads_flex_analysis(dict_loads_ts, dict_network, 1, 100)
