0e2d40e8ef6097307cdea18d8ec1a43020091485
Daniel Bjerkehagen <daniel.bjerkehagen@sintef.no>
2022-03-31 10:42:07 +0200

    Fix missing imports

--- src/analysis/methods/coincidence_factors.py
+++ src/analysis/methods/coincidence_factors.py
@@ -1,10 +1,14 @@
 # Aggregation factor: The ratio between a child's load at the point of the node's aggregated max load, and the child's max load
 # Norsk: "Sammenlagringsfaktor"
 
+from load_aggregation import aggregate_load_of_node
+from max_load import find_max_load
+import objects.network as network
+
 def aggregation_factors(str_bus_ID, dict_loads_ts, g_network): 
 
     ts_aggregate = aggregate_load_of_node(str_bus_ID, dict_loads_ts, g_network)
-    fl_max, int_max_index = max_load(ts_aggregate)
+    fl_max, int_max_index = find_max_load(ts_aggregate)
 
     dict_aggregation_factors = {}
     list_children = network.list_children_of_node(str_bus_ID, g_network)
@@ -14,7 +18,7 @@ def aggregation_factors(str_bus_ID, dict_loads_ts, g_network):
         fl_child_load_at_max = ts_child[int_max_index,1]
 
         # find child's max load
-        c_max, c_max_index = max_load(ts_child)
+        c_max, c_max_index = find_max_load(ts_child)
         dict_aggregation_factors[str_child_ID] = fl_child_load_at_max/c_max
 
     return dict_aggregation_factors
@@ -24,7 +28,7 @@ def aggregation_factors(str_bus_ID, dict_loads_ts, g_network):
 def coincidence_factors(str_bus_ID, dict_loads_ts, g_network): 
 
     ts_aggregate = aggregate_load_of_node(str_bus_ID, dict_loads_ts, g_network)
-    fl_max, int_max_index = max_load(ts_aggregate)
+    fl_max, int_max_index = find_max_load(ts_aggregate)
 
     dict_coincidence_factors = {}
     list_children = network.list_children_of_node(str_bus_ID, g_network)
