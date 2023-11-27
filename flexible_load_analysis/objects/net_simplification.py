e6ce88af69a660c2746d407f1c0ed1ac3037de7c
Daniel Bjerkehagen <daniel.bjerkehagen@sintef.no>
2023-11-27 11:30:52 +0100

    Stop overwriting network and loads when simplifying

--- flexible_load_analysis/objects/net_simplification.py
+++ flexible_load_analysis/objects/net_simplification.py
@@ -1,4 +1,5 @@
 import warnings
+import copy
 
 import numpy as np
 
@@ -20,30 +21,34 @@ def simplify_net(dict_loads, dict_network, unifying_voltage_kV, reference_bus=No
     # We only want to simplify nodes beneath the reference bus (inclusive)
     candidate_nodes = all_buses_below(reference_bus, dict_network, reference_bus) + [reference_bus]
     trafo_bus_pairs = [p for p in trafo_bus_pairs if (p[0] in candidate_nodes and p[1] in candidate_nodes)]
+
+
     # for each pair, aggregate the load at the pair and store in a single timeseries to be
     # associated with the combined node
+    dict_loads_simplified = copy.deepcopy(dict_loads)
+    dict_network_simplified = copy.deepcopy(dict_network)
     nodes_removed_so_far = []
     for (f_bus, t_bus) in trafo_bus_pairs:
-        if network.voltage_for_node_id(f_bus, dict_network) == unifying_voltage_kV:
+        if network.voltage_for_node_id(f_bus, dict_network_simplified) == unifying_voltage_kV:
             inner_node = f_bus
             outer_node = t_bus
-        elif network.voltage_for_node_id(t_bus, dict_network) == unifying_voltage_kV:
+        elif network.voltage_for_node_id(t_bus, dict_network_simplified) == unifying_voltage_kV:
             inner_node = t_bus
             outer_node = f_bus
         else:
             continue
             #raise Exception("Transformer present in network which doesn't match unifying voltage level")
 
-        if network.voltage_for_node_id(outer_node, dict_network) > unifying_voltage_kV:
+        if network.voltage_for_node_id(outer_node, dict_network_simplified) > unifying_voltage_kV:
             # Simplifying upwards a transformer
 
             # Special case when the high voltage side of the transformer is the reference bus
             # Then this can simply be made to the lower voltage level,
             # with impedance corrected
-            if network.is_reference_bus(outer_node, dict_network):
+            if network.is_reference_bus(outer_node, dict_network_simplified):
                 # Reference bus stays as a reference bus
-                network.convert_trafo_branch_to_equivalent_impedance(f_bus, t_bus, dict_network)
-                network.set_voltage_level(outer_node, unifying_voltage_kV, dict_network)
+                network.convert_trafo_branch_to_equivalent_impedance(f_bus, t_bus, dict_network_simplified)
+                network.set_voltage_level(outer_node, unifying_voltage_kV, dict_network_simplified)
 
             # if the high voltage side, however, is connected to a larger network then 
             # this must be handled differently, possibly by simply throwing this part
@@ -59,14 +64,14 @@ def simplify_net(dict_loads, dict_network, unifying_voltage_kV, reference_bus=No
                 continue
             # Simplifying downwards a transformer
             # Here, the highest impedance of a removed node should really be added to the impedance of outer_node...
-            nodes_to_remove = all_buses_below(outer_node, dict_network, reference_bus)
-            new_load_ts = aggregate_load_of_node(outer_node, dict_loads, dict_network, reference_bus)
+            nodes_to_remove = all_buses_below(outer_node, dict_network_simplified, reference_bus)
+            new_load_ts = aggregate_load_of_node(outer_node, dict_loads_simplified, dict_network_simplified, reference_bus)
             for n in nodes_to_remove:
-                net_modification.remove_node_from_net(dict_loads, dict_network, n)
+                net_modification.remove_node_from_net(dict_loads_simplified, dict_network_simplified, n)
             nodes_removed_so_far += nodes_to_remove
-            if np.any(new_load_ts): dict_loads[outer_node] = new_load_ts
+            if np.any(new_load_ts): dict_loads_simplified[outer_node] = new_load_ts
 
-            network.convert_trafo_branch_to_equivalent_impedance(f_bus, t_bus, dict_network)
-            network.set_voltage_level(outer_node, unifying_voltage_kV, dict_network)
+            network.convert_trafo_branch_to_equivalent_impedance(f_bus, t_bus, dict_network_simplified)
+            network.set_voltage_level(outer_node, unifying_voltage_kV, dict_network_simplified)
 
-    return dict_loads, dict_network
+    return dict_loads_simplified, dict_network_simplified
